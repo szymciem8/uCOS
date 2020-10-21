c2e2bbc56647fceff6803ed4765cd44598cec56a
Szymon Ciemała <szymciem8@gmail.com>
2020-10-22 01:25:22 +0200

    Create lab_ucos.c

---
+++ lab_ucos.c
@@ -0,0 +1,51 @@
+/*
+------------------------------SYSTEMY OPERACYJNE--------------------------------
+
+Program polegający, który ma za zadnie obciążać wieloma procesami (task)
+system operacyjny.
+
+Politechnika Śląska
+Wydział Automatyki, Elektroniki i Informatyki
+Laboratorium RTOS 2,3
+
+Autorzy: Szymon Ciemała, Jakub Kaniowski
+*/
+
+//LIBRARIES
+#include "includes.h"
+
+//------------------------------------------------------------------------------
+//                                  CONSTANTS
+//------------------------------------------------------------------------------
+
+#define TASK_STACK_SIZE 512
+#define NUMBER_OF_TASKS 21
+
+//------------------------------------------------------------------------------
+//                              GLOBAL VARIABLES
+//------------------------------------------------------------------------------
+OS_STK TaskStk[N_TASKS][TASK_STK_SIZE];
+OS_STK TaskStartStk[TASK_STK_SIZE];
+
+//------------------------------------------------------------------------------
+//                          PROTOTYPES OF FUNCTIONS
+//------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------
+//                                  MAIN
+//------------------------------------------------------------------------------
+void main(void){
+
+}
+
+
+void display(){
+
+
+}
+
+void read_key(){
+
+
+}
